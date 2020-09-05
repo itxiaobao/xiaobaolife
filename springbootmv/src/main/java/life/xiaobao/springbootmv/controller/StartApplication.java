bff016c4fee46c6cf52eed33c0ddb595947fdf0b
itxiaobao <34150722+itxiaobao@users.noreply.github.com>
2020-09-05 14:33:48 +0800

    基于maven构建的springboot项目

--- springbootmv/src/main/java/life/xiaobao/springbootmv/controller/StartApplication.java
+++ springbootmv/src/main/java/life/xiaobao/springbootmv/controller/StartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartApplication {
 
+    //基于maven构建的springboot项目
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class);
     }
